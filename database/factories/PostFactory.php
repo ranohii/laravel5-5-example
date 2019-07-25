37aa3e21f99c88ac67fd1ea1fc1ca3af79a9bc75
ranohii <lille.alley@gmail.com>
2019-07-25 22:37:57 +0900

    MySQL5.7.7以前のバージョンでは、PRIMARY_KEYおよびUNIQUE_KEYを付けたカラムには最大767bytesまでしか入らない。

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -19,7 +19,7 @@ $factory->define(App\Models\Post::class, function (Faker $faker) {
         'meta_description' => $faker->sentence($nbWords = 6, $variableNbWords = true),
         'meta_keywords' => implode(',', $faker->words($nb = 3, $asText = false)),
         'excerpt' => $faker->paragraph($nbSentences = 4, $variableNbSentences = true),
-        'body' => $faker->paragraphs($nb = 8, $asText = true),
+        'body' => $faker->paragraphs($nb = 4, $asText = true),
         'active' => true,
     ];
 });
